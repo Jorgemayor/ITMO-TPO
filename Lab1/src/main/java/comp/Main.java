e089bce57de58d5e6ca523c2eb45d54cf66e803a
Jorgemayor <jorge.e.mayor@gmail.com>
2024-04-11 17:09:14 +0300

    feat: add task 2

--- Lab1/src/main/java/comp/Main.java
+++ Lab1/src/main/java/comp/Main.java
@@ -1,11 +1,25 @@
 package comp;
 
 import comp.task1.Cos;
+import comp.task2.SplayTree.Node;
+
+import static comp.task2.SplayTree.insert;
+import static comp.task2.SplayTree.printKeys;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Task 1:");
         System.out.println(Cos.calc(50));
-        
+
+        System.out.println("\n\nTask 2:");
+
+        Node root = null;
+        root = insert(root, 100);
+        root = insert(root, 50);
+        root = insert(root, 200);
+        root = insert(root, 40);
+        root = insert(root, 60);
+        System.out.println("Preorder traversal of the modified Splay tree:");
+        printKeys(root);
     }
 }
---
+++ Lab1/src/main/java/comp/task2/SplayTree.java
@@ -0,0 +1,109 @@
+package comp.task2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SplayTree {
+
+    public static class Node {
+        public int key;
+        public Node left, right;
+
+        public static List<Integer> keys;
+
+    }
+
+    private static Node newNode(int key) {
+        Node node = new Node();
+        node.key = key;
+        node.left = node.right = null;
+        return node;
+    }
+
+    private static Node rightRotate(Node x) {
+        Node y = x.left;
+        x.left = y.right;
+        y.right = x;
+        return y;
+    }
+
+    private static Node leftRotate(Node x) {
+        Node y = x.right;
+        x.right = y.left;
+        y.left = x;
+        return y;
+    }
+
+    private static Node splay(Node root, int key) {
+        if (root == null || root.key == key)
+            return root;
+
+        if (root.key > key) {
+            if (root.left == null)
+                return root;
+            if (root.left.key > key) {
+                root.left.left = splay(root.left.left, key);
+                root = rightRotate(root);
+            }
+            else if (root.left.key < key) {
+                root.left.right = splay(root.left.right, key);
+                if (root.left.right != null)
+                    root.left = leftRotate(root.left);
+            }
+            return (root.left == null) ? root : rightRotate(root);
+        }
+        else {
+            if (root.right == null)
+                return root;
+            if (root.right.key > key) {
+                root.right.left = splay(root.right.left, key);
+                if (root.right.left != null)
+                    root.right = rightRotate(root.right);
+            }
+            else if (root.right.key < key) {
+                root.right.right = splay(root.right.right, key);
+                root = leftRotate(root);
+            }
+            return (root.right == null) ? root : leftRotate(root);
+        }
+    }
+
+    public static Node insert(Node root, int key) {
+        if (root == null)
+            return newNode(key);
+
+        root = splay(root, key);
+
+        if (root.key == key)
+            return root;
+
+        Node node = newNode(key);
+        if (root.key > key) {
+            node.right = root;
+            node.left = root.left;
+            root.left = null;
+        }
+        else {
+            node.left = root;
+            node.right = root.right;
+            root.right = null;
+        }
+        return node;
+    }
+
+    private static void setKeys(Node node) {
+        if (node != null) {
+            setKeys(node.left);
+            Node.keys.add(node.key);
+            setKeys(node.right);
+        }
+    }
+
+    public static void printKeys(Node node) {
+        Node.keys = new ArrayList<>();
+        setKeys(node);
+        for (int key : Node.keys) {
+            System.out.println(key);
+        }
+    }
+}
